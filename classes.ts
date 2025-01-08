f7eab84f0e95cf95d5de5506ed1f064ea03041e4
sashanktadimeti <sashanktadimeti4@gmail.com>
2025-01-08 21:39:29 +0530

    added

---
+++ classes.ts
@@ -0,0 +1,11 @@
+class User{
+    email:string
+    name:string
+    city:string="tanuku"
+  constructor(email:string,name:string){
+    this.email=email
+    this.name=name
+  }
+}
+const sashank=new User("sashanktadimeti4@gmail.com","sashank")
+sashank.city="vizag"
